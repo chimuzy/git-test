0be8ced8387333f65ecfc6245a2710954242abed
Chimu <chimuzy@qq.com>
2022-06-25 10:57:35 +0800

    second commit

--- src/main/java/test/git_test.java
+++ src/main/java/test/git_test.java
@@ -3,6 +3,7 @@ package test;
 public class git_test {
     public static void main(String[] args) {
         System.out.println("git test1!!");
+        System.out.println("git test2!!");
     }
 
 }
