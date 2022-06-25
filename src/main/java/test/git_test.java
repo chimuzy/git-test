5b8ba93d1aa5001ae496d18f11905b7b9a3e0728
Chimu <chimuzy@qq.com>
2022-06-25 11:05:06 +0800

    hot-fix commit1

--- src/main/java/test/git_test.java
+++ src/main/java/test/git_test.java
@@ -5,6 +5,7 @@ public class git_test {
         System.out.println("git test1!!");
         System.out.println("git test2!!");
         System.out.println("git test3!!");
+        System.out.println("git test4!!");
     }
 
 }
